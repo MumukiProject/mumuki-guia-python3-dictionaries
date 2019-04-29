9ccc097ad5c8119342e13b9b4ea0d1b4a813632d
Gustavo Trucco <gustavo@mumuki.org>
2019-04-29 18:38:13 -0300

    corrijo codigo extra del ejercicio 3

--- 00003_Accediendo al campo/extra.py
+++ 00003_Accediendo al campo/extra.py
@@ -1,3 +1,3 @@
-mercurio = { nombre: "Mercurio", temperaturaPromedio: 67, tieneAnillos: false };
-marte = { nombre: "Marte", temperaturaPromedio: -63, tieneAnillos: false };
-saturno = { nombre: "Saturno", temperaturaPromedio: -139, tieneAnillos: true };
+mercurio = { "nombre": "Mercurio", "temperaturaPromedio": 67, "tieneAnillos": false };
+marte = { "nombre": "Marte", "temperaturaPromedio": -63, "tieneAnillos": false };
+saturno = { "nombre": "Saturno", "temperaturaPromedio": -139, "tieneAnillos": true };
