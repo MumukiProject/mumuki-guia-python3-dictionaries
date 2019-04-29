eb72d7a49cc2525c6b8cc74871a486143ead52fb
Gustavo Trucco <gustavo@mumuki.org>
2019-04-29 18:43:51 -0300

    Corrijo el código extra del ejercicio 4

--- 00004_Temperatura de planeta/extra.py
+++ 00004_Temperatura de planeta/extra.py
@@ -1,4 +1,4 @@
-mercurio = { nombre: "Mercurio", temperaturaPromedio: 67, tieneAnillos: false };
-marte = { nombre: "Marte", temperaturaPromedio: -63, tieneAnillos: false };
-saturno = { nombre: "Saturno", temperaturaPromedio: -139, tieneAnillos: true };
-venus = { nombre: "Venus", temperaturaPromedio: 462, tieneAnillos: false };
+mercurio = { "nombre": "Mercurio", "temperaturaPromedio": 67, "tieneAnillos": false };
+marte = { "nombre": "Marte", "temperaturaPromedio": -63, "tieneAnillos": false };
+saturno = { "nombre": "Saturno", "temperaturaPromedio": -139, "tieneAnillos": true };
+venus = { "nombre": "Venus", "temperaturaPromedio": 462, "tieneAnillos": false };
