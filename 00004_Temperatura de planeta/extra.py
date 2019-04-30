108ca3a691de51cca4555a9459b8cc772debacea
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-30 18:38:55 +0000

    Mumuki Export on 2019-04-30 18:38:55 +0000

--- 00004_Temperatura de planeta/extra.py
+++ 00004_Temperatura de planeta/extra.py
@@ -1,4 +1,4 @@
-mercurio = { "nombre": "Mercurio", "temperatura_promedio": 67, "tiene_anillos": false }
-marte = { "nombre": "Marte", "temperatura_promedio": -63, "tiene_anillos": false }
-saturno = { "nombre": "Saturno", "temperatura_promedio": -139, "tiene_anillos": true }
-venus = { "nombre": "Venus", "temperatura_promedio": 462, "tiene_anillos": false }
+mercurio = { "nombre": "Mercurio", "temperatura_promedio": 67, "tiene_anillos": False }
+marte = { "nombre": "Marte", "temperatura_promedio": -63, "tiene_anillos": False }
+saturno = { "nombre": "Saturno", "temperatura_promedio": -139, "tiene_anillos": True }
+venus = { "nombre": "Venus", "temperatura_promedio": 462, "tiene_anillos": False }
