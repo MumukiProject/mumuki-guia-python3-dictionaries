946b55e88bb58c2a8e56ae4bfc83cdb98ea25b7d
gustavo@mumuki.org <gustavo@mumuki.org>
2019-04-30 18:35:28 +0000

    Mumuki Export on 2019-04-30 18:35:28 +0000

--- 00003_Accediendo al campo/extra.py
+++ 00003_Accediendo al campo/extra.py
@@ -1,3 +1,3 @@
-mercurio = { "nombre": "Mercurio", "temperatura_promedio": 67, "tiene_anillos": false }
-marte = { "nombre": "Marte", "temperatura_promedio": -63, "tiene_anillos": false }
-saturno = { "nombre": "Saturno", "temperatura_promedio": -139, "tiene_anillos": true }
+mercurio = { "nombre": "Mercurio", "temperatura_promedio": 67, "tiene_anillos": False }
+marte = { "nombre": "Marte", "temperatura_promedio": -63, "tiene_anillos": False }
+saturno = { "nombre": "Saturno", "temperatura_promedio": -139, "tiene_anillos": True }
