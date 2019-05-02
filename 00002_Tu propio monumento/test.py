45d34df3e49ebf379589876d50eab7252fac7586
gustavo@mumuki.org <gustavo@mumuki.org>
2019-05-02 20:14:26 +0000

    Mumuki Export on 2019-05-02 20:14:26 +0000

--- 00002_Tu propio monumento/test.py
+++ 00002_Tu propio monumento/test.py
@@ -2,10 +2,10 @@
   def test_torre_azadi_se_llama_Torre_Azadi(self):
     self.assertEqual(torre_azadi["nombre"].lower(), "torre azadi")
 
-  def test_torre_azadi_es_de_Teherán_Irán(self):
+  def test_torre_azadi_es_de_Teheran_Iran(self):
     self.assertEqual(torre_azadi["locacion"], "Teherán, Irán")
 
-  def test_torre_azadi_tiene_su_año_de_construcción(self):
+  def test_torre_azadi_tiene_su_anio_de_construccion(self):
     self.assertEqual(torre_azadi["anio_de_construccion"], 1971)
 
   def test_monumento_nacional_a_la_bandera_se_llama_Monumento_Nacional_a_la_Bandera(self):
@@ -14,5 +14,5 @@
   def test_monumento_nacional_a_la_bandera_es_de_Rosario_Argentina(self):
     self.assertEqual(monumento_nacional_a_la_bandera["locacion"], "Rosario, Argentina")
 
-  def test_monumento_nacional_a_la_bandera_tiene_su_año_de_construcción(self):
+  def test_monumento_nacional_a_la_bandera_tiene_su_anio_de_construccion(self):
     self.assertEqual(monumento_nacional_a_la_bandera["anio_de_construccion"], 1957)
