9c52cd4062699376b397364201f46758a2a864e3
gustavo@mumuki.org <gustavo@mumuki.org>
2019-05-02 19:44:58 +0000

    Mumuki Export on 2019-05-02 19:44:58 +0000

--- 00007_Postres complejos/test.py
+++ 00007_Postres complejos/test.py
@@ -4,10 +4,10 @@
   cheesecake = { "ingredientes": ["queso crema", "frambuesas"], "tiempo_de_coccion": 80 }
   lemon_pie = { "ingredientes": ["jugo de limón", "almidón de maíz", "leche", "huevos"], "tiempo_de_coccion": 65 }
 
-  def test_el_flan_casero_es_más_difícil_de_cocinar_que_el_cheesecake(self):
+  def test_el_flan_casero_es_mas_difícil_de_cocinar_que_el_cheesecake(self):
     self.assertEqual(mas_dificil_de_cocinar(flan_casero, cheesecake), flan_casero)
   
-  def test_el_lemon_pie_es_más_difícil_de_cocinar_que_el_cheesecake(self):
+  def test_el_lemon_pie_es_mas_difícil_de_cocinar_que_el_cheesecake(self):
     self.assertEqual(mas_dificil_de_cocinar(cheesecake, lemon_pie), lemon_pie)
   
   def test_si_dos_postres_son_igual_de_difíciles_de_cocinar_devuelve_cualquiera_de_los_dos(self):
