58e6acf58855d9bf33148d13e779a7cc642f107b
gustavo@mumuki.org <gustavo@mumuki.org>
2019-05-02 20:33:13 +0000

    Mumuki Export on 2019-05-02 20:33:13 +0000

--- 00009_60 dulces minutos/test.py
+++ 00009_60 dulces minutos/test.py
@@ -1,9 +1,9 @@
 
-  def test_un postre de una hora y media no es rápido(self):
+  def test_un postre_de_una_hora_y_media_no_es_rapido(self):
     postreDeLeche = {"ingredientes":["leche"], "tiempo_de_coccion":90}
     self.assertEqual(len(postres_rapidos), 2)
   
-  def test_un postre de media hora es rápido(self):
+  def test_un postre_de_media_hora_es_rapido(self):
     postreDeLeche = {"ingredientes":["leche"], "tiempo_de_coccion":30}
     agregar_a_postres_rapidos(postreDeLeche)
     self.assertEqual(len(postres_rapidos), 3)
