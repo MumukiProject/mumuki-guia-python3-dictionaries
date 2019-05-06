ae515ea942e87536d7a3f971ea442c59325567e2
gustavo@mumuki.org <gustavo@mumuki.org>
2019-05-06 17:37:04 +0000

    Mumuki Export on 2019-05-06 17:37:04 +0000

--- 00007_Postres complejos/test.py
+++ 00007_Postres complejos/test.py
@@ -1,5 +1,4 @@
 
-  
   flan_casero = { "ingredientes": ["huevos", "leche", "azúcar", "vainilla"], "tiempo_de_coccion": 50 }
   cheesecake = { "ingredientes": ["queso crema", "frambuesas"], "tiempo_de_coccion": 80 }
   lemon_pie = { "ingredientes": ["jugo de limón", "almidón de maíz", "leche", "huevos"], "tiempo_de_coccion": 65 }
