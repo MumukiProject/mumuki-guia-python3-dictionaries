6c93c1eca0ee08629a84a5e848f66e15d27796b6
Gustavo Trucco <gustavo@mumuki.org>
2019-05-08 11:41:42 -0300

    Agrego test a 60 dulces minutos

--- 00009_60 dulces minutos/test.py
+++ 00009_60 dulces minutos/test.py
@@ -11,3 +11,10 @@
     agregar_a_postres_rapidos(postres_rapidos, postre_de_leche)
     self.assertEqual(len(postres_rapidos), 3)
     self.assertEqual(postres_rapidos[-1], postre_de_leche)
+
+  def test_un_postre_de_una_hora_es_rapido(self):
+    postres_rapidos = [ { "ingredientes": ["galletitas", "dulceDeLeche", "crema"], "tiempo_de_coccion": 20 }, { "ingredientes": ["huevos", "leche", "azúcar", "vainilla"], "tiempo_de_coccion": 50 } ]
+    postre_de_leche = {"ingredientes":["leche"], "tiempo_de_coccion": 60}
+    agregar_a_postres_rapidos(postres_rapidos, postre_de_leche)
+    self.assertEqual(len(postres_rapidos), 3)
+    self.assertEqual(postres_rapidos[-1], postre_de_leche)
