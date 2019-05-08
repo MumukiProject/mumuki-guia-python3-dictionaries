1b6343714e6d26c2ea31c1570b5e5f45c515015b
Gustavo Trucco <gustavo@mumuki.org>
2019-05-08 11:50:49 -0300

    Agrego test a azucar

--- 00011_¡Azúcar!/test.py
+++ 00011_¡Azúcar!/test.py
@@ -1,7 +1,14 @@
 
-  def test_endulzar_menu_le_agrega_azucar_al_postre(self):
+  def test_endulzar_menu_le_agrega_azucar_al_postre_si_no_tiene(self):
     menu = {"plato_principal": "bife de lomo", "ensalada": ["papa", "zanahoria", "arvejas"], "postre": { "ingredientes": ["queso crema", "frambuesas"], "tiempo_de_coccion": 80 }}
     endulzar_menu(menu)
     
     self.assertEqual(len(menu["postre"]["ingredientes"]), 3)
+    self.assertEqual(menu["postre"]["ingredientes"][-1], "azúcar")
+
+  def test_endulzar_menu_le_agrega_azucar_al_postre_aun_si_tiene(self):
+    menu = {"plato_principal": "milanesas", "ensalada": ["lechuga", "cebolla"], "postre": { "ingredientes": ["dulce de leche", "vainillas", "azucar"], "tiempo_de_coccion": 60 }}
+    endulzar_menu(menu)
+    
+    self.assertEqual(len(menu["postre"]["ingredientes"]), 4)
     self.assertEqual(menu["postre"]["ingredientes"][-1], "azúcar")
