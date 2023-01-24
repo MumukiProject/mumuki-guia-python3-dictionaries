6fc6562f6e4459d0d24593e58dc913c28ab911ad
franco@mumuki.org <franco@mumuki.org>
2023-01-24 19:31:37 +0000

    Mumuki Export on 2023-01-24 19:31:37 +0000

--- 00007_Postres complejos/test.py
+++ 00007_Postres complejos/test.py
@@ -7,8 +7,12 @@
   def test_el_flan_casero_es_mas_dificil_de_cocinar_que_el_cheesecake(self):
     self.assertEqual(mas_dificil_de_cocinar(self.flan_casero, self.cheesecake), self.flan_casero)
   
-  def test_el_lemon_pie_es_mas_dificil_de_cocinar_que_el_cheesecake(self):
-    self.assertEqual(mas_dificil_de_cocinar(self.cheesecake, self.lemon_pie), self.lemon_pie)
+  def test_el_lemon_pie_es_mas_dificil_de_cocinar_que_el_cheesecake_al_derecho(self):
+    self.assertEqual(mas_dificil_de_cocinar(self.cheesecake, self.lemon_pie), self.lemon_pie, "mas_dificil_de_cocinar(cheescake, lemon_pie) devuelve lemon_pie")
+
+  def test_el_lemon_pie_es_mas_dificil_de_cocinar_que_el_cheesecake_al_reves(self):
+    self.assertEqual(mas_dificil_de_cocinar(self.lemon_pie, self.cheescake), self.lemon_pie, "mas_dificil_de_cocinar(lemon_pie, cheescake) devuelve lemon_pie")
+
   
   def test_si_dos_postres_son_igual_de_dificiles_de_cocinar_devuelve_cualquiera_de_los_dos(self):
     mas_dificil = mas_dificil_de_cocinar(self.flan_casero, self.lemon_pie)
