bc4f097443a15301083dfb341055f62a7624094b
franco@mumuki.org <franco@mumuki.org>
2023-01-24 23:56:31 +0000

    Mumuki Export on 2023-01-24 23:56:31 +0000

--- 00005_Moviendo archivos/test.py
+++ 00005_Moviendo archivos/test.py
@@ -1,7 +1,7 @@
 
   def test_mover_archivo_no_retorna_nada(self):
     archivo = {"ruta":"/usr/miarchivo.doc", "creacion":"15/02/2019"}
-    self.assertEqual(None, mover_archivo(archivo, "/home/miarchivo.doc"))
+    self.assertEqual(None, mover_archivo(archivo, "/home/miarchivo.doc"), "No debe retornar nada, dado que es un procedimiento")
     
   def test_mover_archivo_le_cambia_la_ruta_a_un_archivo_al_pasar_una_ruta_nueva(self):
     archivo = {"ruta":"/usr/miarchivo.doc", "creacion":"15/02/2019"}
