b2d2f20ea884562602ee573ddba88f71230a7d0a
franco@mumuki.org <franco@mumuki.org>
2023-01-24 19:34:48 +0000

    Mumuki Export on 2023-01-24 19:34:48 +0000

--- 00007_Postres complejos/test.py
+++ 00007_Postres complejos/test.py
@@ -11,10 +11,10 @@
     self.assertEqual(mas_dificil_de_cocinar(self.cheesecake, self.flan_casero), self.flan_casero, "mas_dificil_de_cocinar(cheesecake, flan_casero) debe devolver flan_casero")
 
   def test_el_lemon_pie_es_mas_dificil_de_cocinar_que_el_cheesecake_al_derecho(self):
-    self.assertEqual(mas_dificil_de_cocinar(self.cheesecake, self.lemon_pie), self.lemon_pie, "mas_dificil_de_cocinar(cheescake, lemon_pie) debe devolver lemon_pie")
+    self.assertEqual(mas_dificil_de_cocinar(self.cheesecake, self.lemon_pie), self.lemon_pie, "mas_dificil_de_cocinar(cheesecake, lemon_pie) debe devolver lemon_pie")
 
   def test_el_lemon_pie_es_mas_dificil_de_cocinar_que_el_cheesecake_al_reves(self):
-    self.assertEqual(mas_dificil_de_cocinar(self.lemon_pie, self.cheescake), self.lemon_pie, "mas_dificil_de_cocinar(lemon_pie, cheescake) debe devolver lemon_pie")
+    self.assertEqual(mas_dificil_de_cocinar(self.lemon_pie, self.cheesecake), self.lemon_pie, "mas_dificil_de_cocinar(lemon_pie, cheesecake) debe devolver lemon_pie")
 
   def test_si_dos_postres_son_igual_de_dificiles_de_cocinar_devuelve_cualquiera_de_los_dos(self):
     mas_dificil = mas_dificil_de_cocinar(self.flan_casero, self.lemon_pie)
