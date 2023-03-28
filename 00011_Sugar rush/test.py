359d6b4b2aa78f48b863ad2bf690c4804cd4e799
fedescarpa@mumuki.org <fedescarpa@mumuki.org>
2023-03-28 18:58:46 +0000

    Mumuki Export on 2023-03-28 18:58:46 +0000

--- 00011_Sugar rush/test.py
+++ 00011_Sugar rush/test.py
@@ -1,14 +1,14 @@
 
-  def test_endulzar_menu_le_agrega_azucar_al_postre_si_no_tiene(self):
-    menu = {"plato_principal": "bife de lomo", "ensalada": ["papa", "zanahoria", "arvejas"], "postre": { "ingredientes": ["queso crema", "frambuesas"], "tiempo_de_coccion": 80 }}
-    endulzar_menu(menu)
+  def test_sweeten_menu_add_sugar_to_dessert_if_sugar_not_present(self):
+    menu = {"main_dish": "bife de lomo", "salad": ["papa", "zanahoria", "arvejas"], "postre": { "ingredients": ["cream cheese", "raspberries"], "cook_time": 80 }}
+    sweeten_menu(menu)
     
-    self.assertEqual(len(menu["postre"]["ingredientes"]), 3)
-    self.assertTrue(menu["postre"]["ingredientes"][-1] == "azúcar" or menu["postre"]["ingredientes"][-1] == "azucar")
+    self.assertEqual(len(menu["postre"]["ingredients"]), 3)
+    self.assertTrue(menu["postre"]["ingredients"][-1] == "sugar")
 
-  def test_endulzar_menu_le_agrega_azucar_al_postre_aun_si_tiene(self):
-    menu = {"plato_principal": "milanesas", "ensalada": ["lechuga", "cebolla"], "postre": { "ingredientes": ["dulce de leche", "vainillas", "azucar"], "tiempo_de_coccion": 60 }}
-    endulzar_menu(menu)
+  def test_sweeten_menu_add_sugar_to_dessert_if_sugar_present(self):
+    menu = {"main_dish": "milanesas", "salad": ["lechuga", "cebolla"], "postre": { "ingredients": ["dulce de leche", "vanilla", "sugar"], "cook_time": 60 }}
+    sweeten_menu(menu)
     
-    self.assertEqual(len(menu["postre"]["ingredientes"]), 4)
-    self.assertTrue(menu["postre"]["ingredientes"][-1] == "azúcar" or menu["postre"]["ingredientes"][-1] == "azucar")
+    self.assertEqual(len(menu["postre"]["ingredients"]), 4)
+    self.assertTrue(menu["postre"]["ingredients"][-1] == "sugar")
