2ce890293b263d21244dd6371844614c9e092cb1
franco@mumuki.org <franco@mumuki.org>
2023-04-04 20:15:40 +0000

    Mumuki Export on 2023-04-04 20:15:40 +0000

--- 00004_Planet temperature/test.py
+++ 00004_Planet temperature/test.py
@@ -13,3 +13,10 @@
 
   def test_planet_temperature_works_for_any_planet(self):
     self.assertEqual(planet_temperature({"name": "Earth", "average_temperature": 25}), "Earth has an average temperature of 25 degrees")
+
+  def test_has_no_type_errors(self):
+    try:
+      planet_temperature(mars)
+    except TypeError as e:
+      if "can only concatenate str" in str(e): 
+        self.fail("Should not throw TypeError")
