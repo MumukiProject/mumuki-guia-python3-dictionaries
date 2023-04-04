18a245dfb9b5b712e92122362a630725a6adf5c2
franco@mumuki.org <franco@mumuki.org>
2023-04-04 20:10:13 +0000

    Mumuki Export on 2023-04-04 20:10:13 +0000

--- 00004_Planet temperature/extra.py
+++ 00004_Planet temperature/extra.py
@@ -1,4 +1,4 @@
-marcury = { "name": "Mercury", "average_temperature": 67, "tiene_anillos": False }
+mercury = { "name": "Mercury", "average_temperature": 67, "tiene_anillos": False }
 mars = { "name": "Mars", "average_temperature": -63, "tiene_anillos": False }
 saturn = { "name": "Saturn", "average_temperature": -139, "tiene_anillos": True }
 venus = { "name": "Venus", "average_temperature": 462, "tiene_anillos": False }
