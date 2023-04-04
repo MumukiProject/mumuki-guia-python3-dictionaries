4b639cfdb2256564dbb7fe1f33551abc60864e9d
franco@mumuki.org <franco@mumuki.org>
2023-04-04 20:03:07 +0000

    Mumuki Export on 2023-04-04 20:03:07 +0000

--- 00003_Fields of Mars/extra.py
+++ 00003_Fields of Mars/extra.py
@@ -1,6 +1,6 @@
-mercurio = { "nombre": "Mercurio", "temperatura_promedio": 67, "tiene_anillos": False }
-marte = { "nombre": "Marte", "temperatura_promedio": -63, "tiene_anillos": False }
-saturno = { "nombre": "Saturno", "temperatura_promedio": -139, "tiene_anillos": True }
+mercury = { "name": "Mercury", "average_temperature": 67, "has_rings": False }
+mars = { "name": "Mars", "average_temperature": -63, "has_rings": False }
+saturn = { "name": "Saturn", "average_temperature": -139, "has_rings": True }
 
 def done():
   pass
