47476540690e90146fdf4115a476b0b134278da7
franco@mumuki.org <franco@mumuki.org>
2023-04-05 14:36:44 +0000

    Mumuki Export on 2023-04-05 14:36:44 +0000

--- 00011_Sugar rush/test.py
+++ 00011_Sugar rush/test.py
@@ -1,14 +1,14 @@
 
   def test_sweeten_menu_add_sugar_to_dessert_if_sugar_not_present(self):
-    menu = {"main_dish": "bife de lomo", "salad": ["papa", "zanahoria", "arvejas"], "postre": { "ingredients": ["cream cheese", "raspberries"], "cook_time": 80 }}
+    menu = {"main_dish": "bife de lomo", "salad": ["papa", "zanahoria", "arvejas"], "dessert": { "ingredients": ["cream cheese", "raspberries"], "cook_time": 80 }}
     sweeten_menu(menu)
     
-    self.assertEqual(len(menu["postre"]["ingredients"]), 3)
-    self.assertTrue(menu["postre"]["ingredients"][-1] == "sugar")
+    self.assertEqual(len(menu["dessert"]["ingredients"]), 3)
+    self.assertTrue(menu["dessert"]["ingredients"][-1] == "sugar")
 
   def test_sweeten_menu_add_sugar_to_dessert_if_sugar_present(self):
-    menu = {"main_dish": "milanesas", "salad": ["lechuga", "cebolla"], "postre": { "ingredients": ["dulce de leche", "vanilla", "sugar"], "cook_time": 60 }}
+    menu = {"main_dish": "milanesas", "salad": ["lechuga", "cebolla"], "dessert": { "ingredients": ["dulce de leche", "vanilla", "sugar"], "cook_time": 60 }}
     sweeten_menu(menu)
     
-    self.assertEqual(len(menu["postre"]["ingredients"]), 4)
-    self.assertTrue(menu["postre"]["ingredients"][-1] == "sugar")
+    self.assertEqual(len(menu["dessert"]["ingredients"]), 4)
+    self.assertTrue(menu["dessert"]["ingredients"][-1] == "sugar")
